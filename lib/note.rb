c7f50c65f508c899e438b855d2d5de18ab7c4507
Rob Marlow <robertjmarlow@gmail.com>
2020-10-07 14:54:24 -0500

    Autofixing a bunch of rubocop stuff

--- lib/note.rb
+++ lib/note.rb
@@ -1,27 +1,31 @@
+# frozen_string_literal: true
+
 class Note
-    @@all_tones = [:a, :b, :c, :d, :e, :f, :g]
-    @@all_accidetals = [:sharp, :natural, :flat]
+  @@all_tones = %i[a b c d e f g]
+  @@all_accidetals = %i[sharp natural flat]
 
-    attr_reader :tone
-    attr_reader :accidental
+  attr_reader :tone
+  attr_reader :accidental
 
-    def initialize(tone:, accidental: :natural)
-        raise ArgumentError, "#{tone} is not a valid tone. Tones can only be A-G." unless @@all_tones.include?(tone)
-        raise ArgumentError, "#{accidental} is not a valid accidental. Accidentals can only be sharp, natural, or flat." unless @@all_accidetals.include?(accidental)
-
-        @tone = tone
-        @accidental = accidental
+  def initialize(tone:, accidental: :natural)
+    raise ArgumentError, "#{tone} is not a valid tone. Tones can only be A-G." unless @@all_tones.include?(tone)
+    unless @@all_accidetals.include?(accidental)
+      raise ArgumentError, "#{accidental} is not a valid accidental. Accidentals can only be sharp, natural, or flat."
     end
 
-    def to_s
-        note_string = "#{tone.upcase}"
+    @tone = tone
+    @accidental = accidental
+  end
 
-        if (@accidental == :flat)
-            note_string << '♭'
-        elsif (@accidental == :sharp)
-            note_string << '♯'
-        end
+  def to_s
+    note_string = tone.upcase.to_s
 
-        return note_string
+    if @accidental == :flat
+      note_string << '♭'
+    elsif @accidental == :sharp
+      note_string << '♯'
     end
+
+    note_string
+  end
 end
