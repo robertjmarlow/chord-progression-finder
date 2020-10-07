5ea665dd7a934fe2810e9bf3d2bd502941441fa1
Rob Marlow <robertjmarlow@gmail.com>
2020-10-07 16:29:22 -0500

    Documentation is cool

--- lib/key.rb
+++ lib/key.rb
@@ -4,6 +4,9 @@
 class Key
   attr_reader :note, :interval
 
+  # @param note [Note] The note of the key.
+  # @param interval [Symbol] Whether the key is major or minor.
+  # @raise [ArgumentError] if the interval is anything except major or minor.
   def initialize(note:, interval: :major)
     unless %i[major minor].include?(interval)
       raise ArgumentError, "#{interval} is not a valid interval. Intervals can only be major or minor."
