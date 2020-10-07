44a3cabd94b9875e5d93a01113214e09258156ea
Rob Marlow <robertjmarlow@gmail.com>
2020-10-07 14:50:04 -0500

    Fixing a bunch of rubocop stuff

--- lib/note.rb
+++ lib/note.rb
@@ -1,27 +1,27 @@
 class Note
-    @@allTones = [:a, :b, :c, :d, :e, :f, :g]
-    @@allAccidetals = [:sharp, :natural, :flat]
+    @@all_tones = [:a, :b, :c, :d, :e, :f, :g]
+    @@all_accidetals = [:sharp, :natural, :flat]
 
     attr_reader :tone
     attr_reader :accidental
 
     def initialize(tone:, accidental: :natural)
-        raise ArgumentError, "#{tone} is not a valid tone. Tones can only be A-G." unless @@allTones.include?(tone)
-        raise ArgumentError, "#{accidental} is not a valid accidental. Accidentals can only be sharp, natural, or flat." unless @@allAccidetals.include?(accidental)
+        raise ArgumentError, "#{tone} is not a valid tone. Tones can only be A-G." unless @@all_tones.include?(tone)
+        raise ArgumentError, "#{accidental} is not a valid accidental. Accidentals can only be sharp, natural, or flat." unless @@all_accidetals.include?(accidental)
 
         @tone = tone
         @accidental = accidental
     end
 
     def to_s
-        noteString = "#{tone.upcase}"
+        note_string = "#{tone.upcase}"
 
         if (@accidental == :flat)
-            noteString << "♭"
+            note_string << '♭'
         elsif (@accidental == :sharp)
-            noteString << "♯"
+            note_string << '♯'
         end
 
-        return noteString
+        return note_string
     end
 end
--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -1,35 +1,35 @@
 require 'note'
 
 RSpec.describe Note do
-    it "creates a C natural" do
+    it 'creates a C natural' do
         note = Note.new(tone: :c)
 
         expect(note.tone).to eq(:c)
         expect(note.accidental).to eq(:natural)
-        expect(note.to_s).to eq("C")
+        expect(note.to_s).to eq('C')
     end
 
-    it "creates a B flat" do
+    it 'creates a B flat' do
         note = Note.new(tone: :b, accidental: :flat)
-  
+
         expect(note.tone).to eq(:b)
         expect(note.accidental).to eq(:flat)
-        expect(note.to_s).to eq("B♭")
+        expect(note.to_s).to eq('B♭')
     end
 
-    it "creates an F sharp" do
+    it 'creates an F sharp' do
         note = Note.new(tone: :f, accidental: :sharp)
-  
+
         expect(note.tone).to eq(:f)
         expect(note.accidental).to eq(:sharp)
-        expect(note.to_s).to eq("F♯")
+        expect(note.to_s).to eq('F♯')
     end
 
-    it "throws an exception if an illegal tone is specified" do
-        expect {Note.new(tone: :h)}.to raise_error(ArgumentError)
+    it 'throws an exception if an illegal tone is specified' do
+        expect { Note.new(tone: :h) }.to raise_error(ArgumentError)
     end
 
-    it "throws an exception if an illegal accidental is specified" do
-        expect {Note.new(tone: :a, accidental: :super_sharp)}.to raise_error(ArgumentError)
+    it 'throws an exception if an illegal accidental is specified' do
+        expect { Note.new(tone: :a, accidental: :super_sharp) }.to raise_error(ArgumentError)
     end
 end
